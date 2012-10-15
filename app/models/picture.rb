8445e9a0fcc0235971242fa3605dfc683a2adbf1
Paul Zupan <pzupan@gmail.com>
2012-10-15 10:22:30 -0700

    add ckeditor

---
+++ app/models/picture.rb
@@ -0,0 +1,19 @@
+class Picture < Asset
+
+	validates_attachment_size :attachment, :less_than => 2.megabytes
+	
+	def url_content
+	  url(:content)
+	end
+	
+	def url_thumb
+	  url(:thumb)
+	end
+	
+	def to_json(options = {})
+	  options[:methods] ||= []
+	  options[:methods] << :url_content
+	  options[:methods] << :url_thumb
+	  super options
+  end
+end
